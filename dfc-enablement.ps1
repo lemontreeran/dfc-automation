908e989ba20adb9027aa28aefad6706aab297e82
lemontreeran <ran.xing@gmail.com>
2023-09-15 08:59:14 +1000

    Update dfc script

--- dfc-enablement.ps1
+++ dfc-enablement.ps1
@@ -1,43 +1,40 @@
-function Enable-DFC {
-    param (
-        [Parameter(Mandatory = $true)]
-        [String]$Region,
+param (
+    [Parameter(Mandatory = $true)]
+    [String]$Region,
 
-        [Parameter(Mandatory = $true)]
-        [String]$SubscriptionId,
+    [Parameter(Mandatory = $true)]
+    [String]$SubscriptionId,
 
-        [Parameter(Mandatory = $true)]
-        [String]$PricingTier
+    [Parameter(Mandatory = $true)]
+    [String]$PricingTier
 
-        # [Parameter(Mandatory = $true)]
-        # [String]$ParametersFilePath
-    )
+    # [Parameter(Mandatory = $true)]
+    # [String]$ParametersFilePath
+)
 
-    Set-AzContext -Subscription $SubscriptionId
+Set-AzContext -Subscription $SubscriptionId
 
-    # Write-Output "Deploying to $SubscriptionId in $Region using $ConfigurationFilePath"
+# Write-Output "Deploying to $SubscriptionId in $Region using $ConfigurationFilePath"
 
-    $Plans = @(
-        'StorageAccounts'
-    )
+$Plans = @(
+    'StorageAccounts'
+)
 
-    foreach ($Plan in $Plans) {
-        $TemplateParameterObject = @{
-            pricingTier = $PricingTier
-            pricingName = $Plan
-        }
-
-        $ParametersFileName = $Plan.ToLower()
-        $ParametersFilePath = "parameters/$($ParametersFileName).json"
-
-        Write-Output "Deployment Parameters File Path: $ParametersFilePath"
-
-        New-AzSubscriptionDeployment `
-          -Location $Region `
-          -TemplateFile "./dfc-plan.bicep" `
-          # -TemplateParameterObject $TemplateParameterObject `
-          -TemplateParameterFile $ParametersFilePath
-          -Verbose
+foreach ($Plan in $Plans) {
+    $TemplateParameterObject = @{
+        pricingTier = $PricingTier
+        pricingName = $Plan
     }
 
+    $ParametersFileName = $Plan.ToLower()
+    $ParametersFilePath = "parameters/$($ParametersFileName).json"
+
+    Write-Output "Deployment Parameters File Path: $ParametersFilePath"
+
+    New-AzSubscriptionDeployment `
+      -Location $Region `
+      -TemplateFile "./dfc-plan.bicep" `
+      # -TemplateParameterObject $TemplateParameterObject `
+      -TemplateParameterFile $ParametersFilePath
+      -Verbose
 }
